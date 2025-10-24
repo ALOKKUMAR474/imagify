38a2515e1e3f805d527f69e46caf9fcef39f5109
Sward-gs <alokkumarrajawat47@gmail.com>
2025-10-24 23:39:06 +0530

    reset the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import{Routes,Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Buycredit from './pages/Buycredit.jsx'
+import Result from './pages/Result.jsx'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/buycredit' element={<Buycredit/>}/>
+        <Route path='/result' element={<Result/>}/>
+      </Routes>
     </div>
   )
 }
