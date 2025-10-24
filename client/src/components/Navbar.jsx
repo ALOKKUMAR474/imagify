fd696779baabe31458e226d43004622ec6e74b88
Sward-gs <alokkumarrajawat47@gmail.com>
2025-10-25 00:49:02 +0530

    reset the project

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { useState } from 'react';
 import {assets} from '../assets/assets.js'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
-    const [user,setUser] = useState(null);
+    const [user, setUser] = useState(null);
 
   return (
     <div className='flex items-center justify-center py-4'>
@@ -16,9 +17,9 @@ const Navbar = () => {
         {user ? 
          <div></div>
         :
-         <div className='flex items-center gap-5'>
-            <p>pricing</p>
-            <button>Login</button>
+         <div className='flex items-center gap-2 sm:gap-s'>
+            <p className='cursor-pointer'>pricing</p>
+            <button className='bg-zinc-800 text-white px-7 py-2 sm:px-2 text-sm rounded-full'>Login</button>
          </div>
          }
        
